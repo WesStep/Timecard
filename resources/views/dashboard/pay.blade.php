8ae3afcf75cafa25cfedb86bcc302fc865c90704
Wes Stephenson <wesstep1315@gmail.com>
2018-04-07 11:37:20 -0600

    Rearranging elements on the page.

--- resources/views/dashboard/pay.blade.php
+++ resources/views/dashboard/pay.blade.php
@@ -90,25 +90,26 @@
         <div class="row">
             <div class="col-md"></div>
             <div class="col-md"></div>
-            <form class="row" action="#" method="post">
-                <div class="col-md">
-                        <div class="form-group">
-                            <div class="checkbox">
-                                <label class="checkbox-container no-margin">
-                                    <input type="checkbox" name="paid"> Paid?
-                                    <span class="checkmark"></span>
-                                </label>
-                            </div>
-                        </div>
-                </div>
-                <div class="col-md">
-                    {{ csrf_field() }}
-                    <div class="form-group">
-                        <button type="submit" name="pay" class="btn btn-lg btn-primary no-margin">Record</button>
-                    </div>
-                </div>
-            </form>
             <div class="col-md"></div>
+            <div class="col-md"></div>
+            <div class="col-md">
+                <form action="#" method="post">
+                    <div class="form-group row">
+                        <div class="col-md-8 offset-col-4 checkbox">
+                            <label class="checkbox-container no-margin">
+                                <input type="checkbox" name="paid"> Paid?
+                                <span class="checkmark"></span>
+                            </label>
+                        </div>
+                    </div>
+                    {{ csrf_field() }}
+                    <div class="form-group row">
+                        <div class="col-md">
+                            <button type="submit" name="pay" class="btn btn-lg btn-primary no-margin">Record</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
             <div class="col-md-1"></div>
         </div>
         @endisset
