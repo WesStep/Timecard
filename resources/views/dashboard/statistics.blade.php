8521d3a92749681be29d955de5681c2c3274b7cc
Wes <wes.stepehenson@uk2group.com>
2018-04-12 16:06:22 -0600

    Made visual changes to the stats page.

--- resources/views/dashboard/statistics.blade.php
+++ resources/views/dashboard/statistics.blade.php
@@ -9,49 +9,87 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-1">
-                <h3>Details:</h3>
-            </div>
-            <div class="col-md-11">
-                <h1>Week of the {{ $weekAgo->format('jS') }} through the {{ $today->format('jS') }}</h1>
-                <div class="row">
-                    <div class="col-md"></div>
-                    @foreach($companies as $company)
-                        <div class="col-md"><p><strong>{{ $company->name }}</strong></p></div>
-                    @endforeach
-                </div>
+            <div class="col-md-2 bg-info">
+                <h3 class="text-center mt-4">Details:</h3>
                 <hr>
+                <div class="form-group row">
+                    <div class="col-md-12 checkbox">
+                        <label class="checkbox-container no-margin">
+                            <input id="paid" type="checkbox" name="paid"> Notes?
+                            <span class="checkmark"></span>
+                        </label>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-md-12 checkbox">
+                        <label class="checkbox-container no-margin">
+                            <input id="paid" type="checkbox" name="paid"> Clock In/Out Times?
+                            <span class="checkmark"></span>
+                        </label>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-md-12 checkbox">
+                        <label class="checkbox-container no-margin">
+                            <input id="paid" type="checkbox" name="paid"> Duration?
+                            <span class="checkmark"></span>
+                        </label>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-md-12 checkbox">
+                        <label class="checkbox-container no-margin">
+                            <input id="paid" type="checkbox" name="paid"> Paid?
+                            <span class="checkmark"></span>
+                        </label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 text-center">
+                        <button class="btn mr-0 mb-4 btn-dark">Print</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-10">
+                <h1 class="text-center">Week of the {{ $weekAgo->format('jS') }} through the {{ $today->format('jS') }}</h1>
+                <div class="row">
+                    <div class="col-md">
+                        <ul class="nav nav-tabs">
+                            @foreach($companies as $company)
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">{{ $company->name }}</a>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
                 @foreach($users as $user)
                     <div class="row">
-                        <div class="col-md text-md-right">
+                        <div class="col-md-2 text-md-right">
                             <p><strong>{{ $user->name }} ID: {{ $user->id }}</strong></p>
                         </div>
-                        @foreach($companies as $company)
-                            <div class="col-md">
-                                <div class="row">
-                                    <p>Hours worked:</p>
-                                </div>
-                                <div class="row">
-                                    <div class="dropdown">
-                                        <button class="btn btn-info dropdown-toggle" data-toggle="dropdown">Notes</button>
-                                        <div class="dropdown-menu">
-                                            <div class="dropdown-item">
-                                                <hr>
-                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, adipisci amet blanditiis dolores ea eveniet illo ipsa itaque magni maiores non</p>
-                                            </div>
-                                            <div class="dropdown-item">
-                                                <hr>
-                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, adipisci amet blanditiis dolores ea eveniet illo ipsa itaque magni maiores non</p>
-                                            </div>
-                                            <div class="dropdown-item">
-                                                <hr>
-                                                <p>example note</p>
-                                            </div>
+                        <div class="col-md-10">
+                            <div class="row">
+                                <p>Hours worked: 20</p>
+                                <div class="dropdown">
+                                    <button class="btn btn-info dropdown-toggle" data-toggle="dropdown">Notes</button>
+                                    <div class="dropdown-menu">
+                                        <div class="dropdown-item">
+                                            <hr>
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, adipisci amet blanditiis dolores ea eveniet illo ipsa itaque magni maiores non</p>
+                                        </div>
+                                        <div class="dropdown-item">
+                                            <hr>
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, adipisci amet blanditiis dolores ea eveniet illo ipsa itaque magni maiores non</p>
+                                        </div>
+                                        <div class="dropdown-item">
+                                            <hr>
+                                            <p>example note</p>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        @endforeach
+                        </div>
                     </div>
                 <hr>
             @endforeach
