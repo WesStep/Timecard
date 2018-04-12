2d1409ffb3a2522e71b1c430db6e031d7149231b
Wes <wes.stepehenson@uk2group.com>
2018-04-12 15:01:53 -0600

    added importing for vue and axios on the master file, and made updates to the stats page.

--- resources/views/dashboard/statistics.blade.php
+++ resources/views/dashboard/statistics.blade.php
@@ -9,56 +9,53 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="btn-group" role="group" aria-label="Time Periods">
-                <button type="button" class="btn btn-info" onclick="{{ route('dashboard/statistics/week') }}">Last Week</button>
-                <button type="button" class="btn btn-info" onclick="{{ route('dashboard/statistics/month') }}">Last Month</button>
-                <button type="button" class="btn btn-info" onclick="{{ route('dashboard/statistics/year') }}">Last Year</button>
+            <div class="col-md-1">
+                <h3>Details:</h3>
             </div>
-        </div>
-        <div class="row">
-            <h1>Week of the {{ $weekAgo->format('jS') }} through the {{ $today->format('jS') }}</h1>
-        </div>
-        <div class="row">
-            <div class="col-md"></div>
-            @foreach($companies as $company)
-                <div class="col-md"><p><strong>{{ $company->name }}</strong></p></div>
-            @endforeach
-        </div>
-        <hr>
-        @foreach($users as $user)
-        <div class="row">
-            <div class="col-md text-md-right">
-                <p><strong>{{ $user->name }} ID: {{ $user->id }}</strong></p>
-            </div>
-            @foreach($companies as $company)
-                <div class="col-md">
+            <div class="col-md-11">
+                <h1>Week of the {{ $weekAgo->format('jS') }} through the {{ $today->format('jS') }}</h1>
+                <div class="row">
+                    <div class="col-md"></div>
+                    @foreach($companies as $company)
+                        <div class="col-md"><p><strong>{{ $company->name }}</strong></p></div>
+                    @endforeach
+                </div>
+                <hr>
+                @foreach($users as $user)
                     <div class="row">
-                        <p>Hours worked:</p>
-                    </div>
-
-                    <div class="row">
-                        <div class="dropdown">
-                            <button class="btn btn-info dropdown-toggle" data-toggle="dropdown">Notes</button>
-                            <div class="dropdown-menu">
-                                <div class="dropdown-item">
-                                    <hr>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, adipisci amet blanditiis dolores ea eveniet illo ipsa itaque magni maiores non</p>
+                        <div class="col-md text-md-right">
+                            <p><strong>{{ $user->name }} ID: {{ $user->id }}</strong></p>
+                        </div>
+                        @foreach($companies as $company)
+                            <div class="col-md">
+                                <div class="row">
+                                    <p>Hours worked:</p>
                                 </div>
-                                <div class="dropdown-item">
-                                    <hr>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, adipisci amet blanditiis dolores ea eveniet illo ipsa itaque magni maiores non</p>
-                                </div>
-                                <div class="dropdown-item">
-                                    <hr>
-                                    <p>example note</p>
+                                <div class="row">
+                                    <div class="dropdown">
+                                        <button class="btn btn-info dropdown-toggle" data-toggle="dropdown">Notes</button>
+                                        <div class="dropdown-menu">
+                                            <div class="dropdown-item">
+                                                <hr>
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, adipisci amet blanditiis dolores ea eveniet illo ipsa itaque magni maiores non</p>
+                                            </div>
+                                            <div class="dropdown-item">
+                                                <hr>
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, adipisci amet blanditiis dolores ea eveniet illo ipsa itaque magni maiores non</p>
+                                            </div>
+                                            <div class="dropdown-item">
+                                                <hr>
+                                                <p>example note</p>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+                        @endforeach
                     </div>
-                </div>
+                <hr>
             @endforeach
+            </div>
         </div>
-        <hr>
-        @endforeach
     </div>
 @endsection
