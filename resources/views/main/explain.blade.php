d7f654c89cae533da93ebf638cfcbe5d13be249a
Wes Stpehenson <wesstep1315@gmail.com>
2019-01-04 18:12:35 -0700

    Made more updates to the explanation page.

--- resources/views/main/explain.blade.php
+++ resources/views/main/explain.blade.php
@@ -18,7 +18,7 @@
 							<p class="alert alert-success">{{ Session::get('info') }}</p>
 						@endif
 						<h3>General Idea</h3>
-						<p>Timecard.wesstep.com is a project I made in college. The original version of this app was made for a client who uses this software for his companys' employees. There are three main dashboards to login to: one for <strong>employees</strong>, one for <strong>payroll admins</strong>, and one for the <strong>business owner(s)</strong>.</p>
+						<p>Timecard.wesstep.com is a project I made in college. The original version of this app was made for a client who uses this software for his companys' employees. There are three main dashboards to login to: one for <strong>employees</strong>, one for <strong>payroll admins</strong>, and one for <strong>business owners</strong>.</p>
 						<p>You can access each of these respective accounts by using the following credentials:</p>
 						<div class="list-group">
 							<div class="list-group-item list-group-item-action flex-column align-items-start">
@@ -81,8 +81,8 @@
 			<div class="col-md-8">
 				<div class="card card-default mb-4">
 					<div class="card-body">
-						<h3>Make sure it will work</h3>
-						<p>As this is a public-facing site and credentials are on this page, anyone can access this app and create fictional shifts, companies, and other data. It is important that, before you go ahead and test out the app, you reset any data stored in the database first.</p>
+						<h1>Make sure it will work</h1>
+						<p>As this is a public-facing site and the login credentials are on this page, anyone can access this app and create fictional shifts, companies, and other data. It is important that, before you go ahead and test out the app, you reset any data stored in the database first.</p>
 						<p>Reset the database <a href="{{ route('reset') }}">Here</a>.</p>
 						@if(Session::has('info'))
 							<p class="alert alert-success">{{ Session::get('info') }}</p>
